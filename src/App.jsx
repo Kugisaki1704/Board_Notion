42fd072be80f09267ffc6036f273213b2c0a3328
Kugisaki1704 <sapnabora85@gmail.com>
2024-03-09 23:32:09 +0530

    added drag and drop

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import StartingPage from './components/StartingPage';
 import ProjectBoard from './components/ProjectBoard';
 import { Toaster } from 'react-hot-toast';
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 
 function App(){
   const [tasks, setTasks] = useState([]);
@@ -13,15 +15,14 @@ function App(){
 
 
   return (
-    <>
+    <DndProvider backend={HTML5Backend}>
     <Toaster />
     <div className="w-screen h-screen flex flex-col items-center p-3 gap-16 pt-32">
       <StartingPage tasks={tasks} setTasks={setTasks}/> 
       <ProjectBoard tasks={tasks} setTasks={setTasks}/> 
       {/* props */}
     </div>
-    
-    </>
+    </DndProvider>
   )
 }
 
