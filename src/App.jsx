88bfc2e500341718fc5ebe3dd7823d098ec1f25c
Kugisaki1704 <sapnabora85@gmail.com>
2024-03-10 11:17:35 +0530

    created layout and added router

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,39 @@
-import React, { useEffect, useState } from 'react';
-import StartingPage from './components/StartingPage';
-import ProjectBoard from './components/ProjectBoard';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 import { Toaster } from 'react-hot-toast';
-import { DndProvider } from 'react-dnd'
-import { HTML5Backend } from 'react-dnd-html5-backend'
-
-function App(){
+import StartingPage from './components/startingPage/StartingPage';
+import ProjectBoard from './components/projectBoard/ProjectBoard';
+import TaskDetail from './components/taskDetail/TaskDetail'; // Import TaskDetail component
+import React, { useEffect, useState } from 'react';
+function App() {
   const [tasks, setTasks] = useState([]);
 
-  console.log("tasks",tasks);
   useEffect(()=>{
     setTasks(JSON.parse(localStorage.getItem("tasks")))
   },[])
 
-
+  const Layout=()=>{
+    return(
+      <div className='w-screen h-screen flex flex-col items-center p-3 gap-16 pt-20'>
+        <StartingPage tasks={tasks} setTasks={setTasks}/>
+        <ProjectBoard tasks={tasks} setTasks={setTasks} />
+      </div>
+    )
+  }
   return (
-    <DndProvider backend={HTML5Backend}>
-    <Toaster />
-    <div className="w-screen h-screen flex flex-col items-center p-3 gap-16 pt-32">
-      <StartingPage tasks={tasks} setTasks={setTasks}/> 
-      <ProjectBoard tasks={tasks} setTasks={setTasks}/> 
-      {/* props */}
-    </div>
-    </DndProvider>
-  )
+    <Router>
+      <DndProvider backend={HTML5Backend}>
+        <Toaster />
+        <div>
+          <Routes>
+            <Route path="/" element={<Layout/>} />
+            <Route path="/taskDetail/:id" element={<TaskDetail tasks={tasks} setTasks={setTasks} />} />
+          </Routes>
+        </div>
+      </DndProvider>
+    </Router>
+  );
 }
 
 export default App;
